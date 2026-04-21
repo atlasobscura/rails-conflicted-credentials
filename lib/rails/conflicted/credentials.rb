8722d25731e7fa53679387481a292de976cbf55b
Arthur Lewis <arthur.lewis@gmail.com>
2026-04-21 10:55:31 -0400

    Stop requiring Rails::Credentials::Command
    Testing. Does this fix the bug?

--- lib/rails/conflicted/credentials.rb
+++ lib/rails/conflicted/credentials.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative "credentials/version"
-require_relative "../commands/conflicted_credentials/conflicted_credentials_command"
 
 module Rails
   module Conflicted
